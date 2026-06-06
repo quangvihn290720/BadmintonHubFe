0157a8a8e9cb08b5a2c892827d5faceb1b4b177a
Quang Vinh <72300772+quangvihn290720@users.noreply.github.com>
2026-06-06 17:12:03 +0700

    backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendUrl: (window as any).__BADMINTONHUB_CONFIG__?.backendUrl || 'http://localhost:8080',
-  apiBaseUrl: (window as any).__BADMINTONHUB_CONFIG__?.apiBaseUrl || 'http://localhost:8080/api/v1',
+  backendUrl: (window as any).__BADMINTONHUB_CONFIG__?.backendUrl || 'https://badmintonhubbe.onrender.com',
+  apiBaseUrl: (window as any).__BADMINTONHUB_CONFIG__?.apiBaseUrl || 'https://badmintonhubbe.onrender.com/api/v1',
   useMockApi: false
 };
